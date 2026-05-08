e240e9d00f2649f45cf7b01b4d165d5ed6dbbb43
Juan Pablo Cifuentes <juancifuentestz@gmail.com>
2026-05-08 15:07:20 -0500

    merge resuelto

--- database/init_db.py
+++ database/init_db.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-import sqlite3
-
-def init_db():
-    print("Inicializando base de datos...")
-
-    conn = sqlite3.connect("sares.db")
-
-    with open("database/schema.sql", "r", encoding="utf-8") as f:
-        conn.executescript(f.read())
-
-    conn.close()
-
-def get_connection():
-    conn = sqlite3.connect("sares.db")
-    conn.execute("PRAGMA foreign_keys = ON")
-    return conn
-=======
 import oracledb
 from config import Config
 
@@ -39,4 +21,3 @@ def init_db():
     conn.commit()
     cursor.close()
     conn.close()
->>>>>>> origin/master
